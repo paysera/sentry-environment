7d6c99d5b86877c10b1f8523499521b717718043
Aurimas Salamachinas <a.salamachinas@paysera.com>
2016-09-16 17:30:31 +0300

    Added jira plugin

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -4,6 +4,7 @@
 GIT_BIN_DIRECTORY=/usr/bin
 DOCKER_BIN_DIRECTORY=/usr/bin
 DOCKER_COMPOSER_BIN_DIRECTORY=/usr/local/bin
+SENTRY_INSTALL_JIRA_PLUGIN=false
 
 # Load local config
 if [ -e ".local" ]; then
@@ -16,5 +17,11 @@ ${DOCKER_COMPOSER_BIN_DIRECTORY}/docker-compose up -d --force-recreate
 
 #https://docs.docker.com/compose/startup-order/
 sleep 3
+
+# Install sentry jira plugin
+if [ ${SENTRY_INSTALL_JIRA_PLUGIN} = true ]; then
+    ${DOCKER_BIN_DIRECTORY}/docker exec -it sentryenvironment_sentry_1 pip install sentry-jira
+    ${DOCKER_COMPOSER_BIN_DIRECTORY}/docker-compose restart
+fi
 
 ${DOCKER_BIN_DIRECTORY}/docker exec -it sentryenvironment_sentry_1 sentry upgrade
